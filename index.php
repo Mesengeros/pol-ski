8718f9308e665e5e1855a2da144b51501a32daa6
Mesengeros <adrmaslak@gmail.com>
2023-10-13 09:08:04 +0200

    parcel & bs5 & basic wp files

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<div class="container py-4 px-3 mx-auto">
+  <h1>Hello, Bootstrap and Parcel!</h1>
+  <button class="btn btn-primary">Primary button</button>
+
+</div>
+
+<?php get_footer(); ?>
---
+++ template-parts/card2.php
@@ -0,0 +1,1 @@
+  <button class="btn btn-alert">Primary button</button>
