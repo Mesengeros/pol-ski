ca4245f3936c7b4bfaaad9602f5a8b9df945d5e6
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-10-30 11:11:45 +0100

    first stage

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+   <link rel="stylesheet" href="wp-content/themes/pol-ski/dist/main.css">
+    <script src="wp-content/themes/pol-ski/dist/main.js"></script>
+</head>
+
+<body>
+
+  <div class="container py-4 px-3 mx-auto">
+    <h1>Hello, Bootstrap and Parcel!</h1>
+    <button class="btn btn-primary">Primary button</button>
+    <p class="primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, placeat! Pariatur culpa dolores eligendi veniam aliquam. Excepturi, minima porro eius odit ab repudiandae aliquid aliquam error vero molestias aspernatur perspiciatis?</p>
+  </div>
+
+</body>
+
+</html>
