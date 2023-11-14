0ce95fefbe9cafa63eaeb242ce8bf2564557ed7b
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-11-15 00:09:42 +0100

    added ratio element to the carousel

--- template-parts/home/banner.php
+++ template-parts/home/banner.php
@@ -1,25 +1,25 @@
 <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image1.JPG" class="d-block w-100" alt="image1">
+    <div class="carousel-item active ratio ratio-21x9">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image1.JPG" class="d-block w-100 carousel-image" alt="image1">
     </div>
-    <div class="carousel-item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image2.JPG" class="d-block w-100" alt="image2">
+    <div class="carousel-item ratio ratio-21x9">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image2.JPG" class="d-block w-100 carousel-image" alt="image2">
     </div>
-    <div class="carousel-item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image3.JPG" class="d-block w-100" alt="image3">
+    <div class="carousel-item ratio ratio-21x9">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image3.JPG" class="d-block w-100 carousel-image" alt="image3">
     </div>
-    <div class="carousel-item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image4.JPG" class="d-block w-100" alt="image4">
+    <div class="carousel-item ratio ratio-21x9">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image4.JPG" class="d-block w-100 carousel-image" alt="image4">
     </div>
-    <div class="carousel-item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image5.JPG" class="d-block w-100" alt="image5">
+    <div class="carousel-item ratio ratio-21x9">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image5.JPG" class="d-block w-100 carousel-image" alt="image5">
     </div>
-    <div class="carousel-item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image6.JPG" class="d-block w-100" alt="image6">
+    <div class="carousel-item ratio ratio-21x9">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image6.JPG" class="d-block w-100 carousel-image" alt="image6">
     </div>
-    <div class="carousel-item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image7.JPG" class="d-block w-100" alt="image7">
+    <div class="carousel-item ratio ratio-21x9">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image7.JPG" class="d-block w-100 carousel-image" alt="image7">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
