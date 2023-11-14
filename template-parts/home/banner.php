19b47e116f0a5053d852f7b20ca19560bd44ef4a
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-11-14 14:39:40 +0100

    added carousel on the index page

--- template-parts/home/banner.php
+++ template-parts/home/banner.php
@@ -1,13 +1,25 @@
 <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image1.JPG" class="d-block w-100" alt="image1">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image2.JPG" class="d-block w-100" alt="image2">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image3.JPG" class="d-block w-100" alt="image3">
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image4.JPG" class="d-block w-100" alt="image4">
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image5.JPG" class="d-block w-100" alt="image5">
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image6.JPG" class="d-block w-100" alt="image6">
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image7.JPG" class="d-block w-100" alt="image7">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
