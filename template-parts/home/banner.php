dc6d1c3b6486c4556c63e3d5243657a1d7cfa582
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-11-15 13:10:46 +0100

    styling the banner

--- template-parts/home/banner.php
+++ template-parts/home/banner.php
@@ -1,24 +1,24 @@
 <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
   <div class="carousel-inner">
-    <div class="carousel-item active ratio" style="--bs-aspect-ratio: 50%;">
+    <div class="carousel-item active ratio" style="--bs-aspect-ratio: 30%;">
       <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image1.JPG" class="d-block w-100 carousel-image" alt="image1">
     </div>
-    <div class="carousel-item ratio" style="--bs-aspect-ratio: 50%;">
+    <div class="carousel-item ratio" style="--bs-aspect-ratio: 30%;">
       <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image2.JPG" class="d-block w-100 carousel-image" alt="image2">
     </div>
-    <div class="carousel-item ratio" style="--bs-aspect-ratio: 50%;">
+    <div class="carousel-item ratio" style="--bs-aspect-ratio: 30%;">
       <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image3.JPG" class="d-block w-100 carousel-image" alt="image3">
     </div>
-    <div class="carousel-item ratio" style="--bs-aspect-ratio: 50%;">
+    <div class="carousel-item ratio" style="--bs-aspect-ratio: 30%;">
       <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image4.JPG" class="d-block w-100 carousel-image" alt="image4">
     </div>
-    <div class="carousel-item ratio" style="--bs-aspect-ratio: 50%;">
+    <div class="carousel-item ratio" style="--bs-aspect-ratio: 30%;">
       <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image5.JPG" class="d-block w-100 carousel-image" alt="image5">
     </div>
-    <div class="carousel-item ratio" style="--bs-aspect-ratio: 50%;">
+    <div class="carousel-item ratio" style="--bs-aspect-ratio: 30%;">
       <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image6.JPG" class="d-block w-100 carousel-image" alt="image6">
     </div>
-    <div class="carousel-item ratio" style="--bs-aspect-ratio: 50%;">
+    <div class="carousel-item ratio" style="--bs-aspect-ratio: 30%;">
       <img src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel/image7.JPG" class="d-block w-100 carousel-image" alt="image7">
     </div>
   </div>
